c1df8c3b673bfd93c8ad71d7158c2cb94764b255
aryankumarsingh <aryankumarsingh8340@gmail.com>
2026-02-17 13:50:51 +0530

    update HeroSection minimum height for better layout responsiveness

--- frontend/src/components/home/HeroSection.jsx
+++ frontend/src/components/home/HeroSection.jsx
@@ -42,7 +42,7 @@ export default function HeroSection({
   }, [videoIndex]);
 
   return (
-    <section className="relative w-full min-h-[53vh] md:min-h-[65vh] lg:min-h-[85vh] overflow-hidden">
+    <section className="relative w-full min-h-[35vh] md:min-h-[45vh] lg:min-h-[65vh] overflow-hidden">
 
       {/* VIDEO ONLY (NO OVERLAY) */}
       <video
